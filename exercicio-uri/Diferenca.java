72ecabc5a4c6c870c60e838fccad3f77be2664ec
IltonBJSilva <xlsonplay@gmail.com>
2019-01-17 15:20:21 -0200

    Resolve exercicio URI1015

--- exercicio-uri/Diferenca.java
+++ exercicio-uri/Diferenca.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Nome do autor: Ilton Batista da Silva Júnior 
  * Data de criação do arquivo: 16/01/2019 
  * Objetivo sucinto do programa: mostrar a diferença do produto de A e B pelo produto de C e D
- * Referência ao enunciado/origem do exercício: 
+ * Referência ao enunciado/origem do exercício:https://www.urionlinejudge.com.br/judge/pt/problems/view/1007
  */
 
 public class Diferenca {
